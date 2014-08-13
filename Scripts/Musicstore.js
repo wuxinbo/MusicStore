63bfb195ec8b553542ebb3be949419005f1e4b58
wuxinbo <530286373@qq.com>
2014-08-13 09:42:02 +0800

    third
    购物车同类产品

--- Scripts/Musicstore.js
+++ Scripts/Musicstore.js
@@ -41,48 +41,53 @@ function liupai_position() {
 }
 function addToCart() {
     var album_1 = $.cookie('albums');
-    alert(album_1.length);
-    if (album_1 != null&&album_1.length>0) {
+    
+    if (album_1 != null && album_1.length>=1) {
         var object = JSON.parse(album_1);
         var albums = object;
     } else {
         var albums = new Array();
     }
-    
-
-    albums[albums.length] = new album();
+    //实例化一个对象
+    var album_2 = new album();
+    var id = album_2.id;
+    if (albums.length > 0) {
+        for (var i in albums) {
+            //如果ID相同则说明是同一个产品。
+            if (id == albums[i].id) {
+                albums[i].count = album_2.count;
+            } else {
+                albums[albums.length] = album_2;
+            }
+        }
+    } else {
+        //if(album_2.){}
+        albums[albums.length] = album_2;
+    }
     //JSON.stringify(albums);
     //将js对象转化成json字符串。
+    //alert(albums.length);
     $.cookie('albums', JSON.stringify(albums), {path:"/"});
-    //alert(album_1.length);
+    set_itemVal(albums);
    
 }
+
+//album类的JS版本构造函数
 function album() {
     this.title = $('#biao_value').text();
     this.img = $('#img').attr('src');
-    this.id = $('#biao_value').text();
+    this.id = $('#id').val();
     this.count = $('#amount_value').val();
     this.price = $('#price_value').text();
 }
+//解析COokie中的Json。
 function parse_json_cart() {
     var album_1 = $.cookie('albums');
     
     var object = JSON.parse(album_1);
-    alert(object.length);
-    for (var i in object) {
-             
-        var str = ".item_info_" + i;
-        $(str).find(' #cart_img').attr('src', object[i].img);
-        $(str).find(' #cart_title').text(object[i].title);
-        $(str).find('#item_price').text(object[i].price);
-        $(str).find('#cart_count').text(object[i].count);
-       
-       
-    }
-    $('#counts').text(object.length);
-    $('#total').text('$'+sum(object));
-    $('#p_length').val(object.length);
-    $('#p_counts').text(object.length);
+    //alert(object.length);
+    set_shopping_cart(object);
+    set_itemVal(object);
 }
 //计算商品的总价。
 function sum(ob1) {
@@ -102,6 +107,7 @@ function sum(ob1) {
    
     return total;
 }
+//得到商品对象数组。
 function get_item_array() {
     var album_1 = $.cookie('albums');
     //alert(album_1);
@@ -109,8 +115,28 @@ function get_item_array() {
     return object;
     
 }
+//删除数组中的商品。
 function delete_item(id) {
     var albums = get_item_array();
     var albums =albums.splice(id, 1);
-    $.cookie('albums', albums);
+    $.cookie('albums', JSON.stringify(albums), { path: "/" });
+    set_itemVal(albums);
+}
+//设置购物车商品数目
+function set_itemVal(ob1) {
+    $('#p_counts').text(ob1.length);
+}
+function set_shopping_cart(ob1) {
+    for (var i in ob1) {
+
+        var str = ".item_info_" + i;
+        $(str).find(' #cart_img').attr('src', ob1[i].img);
+        $(str).find(' #cart_title').text(ob1[i].title);
+        $(str).find('#item_price').text(ob1[i].price);
+        $(str).find('#cart_count').text(ob1[i].count);
+        
+    }
+    $('#counts').text(ob1.length);
+    $('#total').text('$' + sum(ob1));
+    $('#p_length').val(ob1.length);
 }
